e0e41559b09c9e7235ed8a1a57c71b40e01f61d8
ann-era <148546178+ann-era@users.noreply.github.com>
2025-02-14 00:54:14 +0800

    Update index.html and index.js
    Update search functionality with API integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+function refreshCurrentWeather(response) {
+  console.log(response.data);
+  let currentTemperatureElement = document.querySelector(
+    "#weather-current-temperature"
+  );
+  let currentTemperature = response.data.temperature.current;
+  currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function refreshCurrentCity(response) {
+  let cityElement = document.querySelector("#weather-current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(searchValue) {
+  city = searchValue.trim(); //.replaceAll(" ", "+");
+  let apiKey = "93aa3f2e40748o3cacebbc6bct7b0422";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshCurrentCity);
+  axios.get(apiUrl).then(refreshCurrentWeather);
+}
+
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#weather-current-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+
+//set default city
+searchCity("Ranau");
